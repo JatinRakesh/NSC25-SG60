b1819578332c6e7c2b34c7db2238c80f9573fec5
JatinRakesh <jatinrakesh15@gmail.com>
2025-03-19 14:34:08 +0800

    nice rect push

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        HStack{
+            NiceRect()
+                    .scaledToFill()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundStyle(.teal)
+                    .frame(width:200, height: 200)
+            NiceRect()
+                    .scaledToFill()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundStyle(.teal)
+                    .frame(width:200, height: 200)
+            NiceRect()
+                    .scaledToFill()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundStyle(.teal)
+                    .frame(width:200, height: 200)
+            NiceRect()
+                    .scaledToFill()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundStyle(.teal)
+                    .frame(width:200, height: 200)
+                    .edgesIgnoringSafeArea(.all)
+            }
+        .edgesIgnoringSafeArea(.all)
+    }
+       
+            }
+        
+    
+
+
+struct NiceRect: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.99566*width, y: 0.5*height))
+        path.addCurve(to: CGPoint(x: 0.50222*width, y: height), control1: CGPoint(x: 0.99566*width, y: height), control2: CGPoint(x: 0.77959*width, y: height))
+        path.addCurve(to: CGPoint(x: 0, y: 0.5*height), control1: CGPoint(x: 0.22485*width, y: height), control2: CGPoint(x: 0, y: height))
+        path.addCurve(to: CGPoint(x: 0.50222*width, y: 0), control1: CGPoint(x: 0, y: 0.1925*height), control2: CGPoint(x: 0.07282*width, y: 0))
+        path.addCurve(to: CGPoint(x: 0.99566*width, y: 0.5*height), control1: CGPoint(x: 0.93162*width, y: 0), control2: CGPoint(x: 0.99566*width, y: 0.20125*height))
+        path.closeSubpath()
+        return path
     }
 }
